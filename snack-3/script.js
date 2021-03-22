6dedbd2b590b85b45ca54446e67883537b745c84
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-22 09:58:05 +0100

    versione con il for

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,12 +1,28 @@
 var numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function thatOne (arr, a, b){
+function thatInterval (arr, a, b){
 
     let onlyNumeriInt = arr.slice(a, b+1);
 
     return onlyNumeriInt;
 }
 
-var slice = thatOne(numeri, 2, 8);
+
+function forThatInterval (arr, a, b){
+
+    for (let i = 0; i < arr.length; i++) {
+         
+        let onlyNumeriInt = [];
+        if (a < arr.indexOf(arr[i]) && b > arr.indexOf(arr[i])){
+            
+            
+            onlyNumeriInt.push(arr[i])
+        }
+;
+        return onlyNumeriInt;
+        
+    }
+}
+var slice = forThatInterval(numeri, 2, 8);
 
 console.log(slice);
